6af17a22ee6920db4cc0ea6287c918e569a7af8d
Bharath Vishnu B <bharathvb1204@gmail.com>
2024-03-17 23:00:58 +0530

    find and replace function added

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
 import sys
-from PyQt5.QtWidgets import QApplication, QMainWindow, QAction, QTextEdit, QFileDialog, QVBoxLayout, QWidget, QLabel
+from PyQt5.QtWidgets import QApplication, QMainWindow, QAction, QTextEdit, QFileDialog, QVBoxLayout, QWidget, QLabel, QDialog, QPushButton, QHBoxLayout, QLineEdit, QMessageBox
 from PyQt5.QtGui import QFont
 from PyQt5.QtCore import Qt
 
@@ -16,15 +16,15 @@ class TextEditor(QMainWindow):
         self.createMenuBar()
 
         # Add status bar to display word count
-        self.statusBar = QLabel("Word count: 0")
-        self.statusBar.setAlignment(Qt.AlignRight)
-        self.setStatusBar(self.statusBar)
+        self.statusBar = self.statusBar()
+        self.wordCountLabel = QLabel("Word count: 0")
+        self.statusBar.addWidget(self.wordCountLabel, 1) 
 
         self.setWindowTitle("Text Editor")
         self.setGeometry(100, 100, 800, 600)
 
         # Connect textChanged signal to updateWordCount slot
-        self.textEdit.textChanged.connect(self.updateWordCount)
+        self.textEdit.textChanged.connect(self.onTextChanged)
 
     def createMenuBar(self):
         menubar = self.menuBar()
@@ -40,6 +40,7 @@ class TextEditor(QMainWindow):
 
         # Add Edit menu with Find and Replace option
         editMenu = menubar.addMenu('&Edit')
+
         findReplaceAction = QAction('&Find and Replace', self)
         findReplaceAction.triggered.connect(self.findAndReplace)
         editMenu.addAction(findReplaceAction)
@@ -59,14 +60,67 @@ class TextEditor(QMainWindow):
                 file.write(text)
 
     def findAndReplace(self):
-        # Implement find and replace functionality here
-        pass
+        dialog = FindReplaceDialog(self)
+        dialog.exec_()
 
-    def updateWordCount(self):
+    def onTextChanged(self):
         # Count the number of words in the text and update the status bar
         text = self.textEdit.toPlainText()
         word_count = len(text.split())
-        self.statusBar.setText(f"Word count: {word_count}")
+        self.wordCountLabel.setText(f"Word count: {word_count}")
+
+
+class FindReplaceDialog(QDialog):
+    def __init__(self, parent=None):
+        super().__init__(parent)
+        self.setWindowTitle("Find and Replace")
+
+        layout = QVBoxLayout()
+
+        find_layout = QHBoxLayout()
+        find_layout.addWidget(QLabel("Find:"))
+        self.find_input = QLineEdit()
+        find_layout.addWidget(self.find_input)
+
+        replace_layout = QHBoxLayout()
+        replace_layout.addWidget(QLabel("Replace with:"))
+        self.replace_input = QLineEdit()
+        replace_layout.addWidget(self.replace_input)
+
+        buttons_layout = QHBoxLayout()
+        find_button = QPushButton("Find")
+        find_button.clicked.connect(self.findText)
+        buttons_layout.addWidget(find_button)
+
+        replace_button = QPushButton("Replace")
+        replace_button.clicked.connect(self.replaceText)
+        buttons_layout.addWidget(replace_button)
+
+        layout.addLayout(find_layout)
+        layout.addLayout(replace_layout)
+        layout.addLayout(buttons_layout)
+
+        self.setLayout(layout)
+
+    def findText(self):
+        text = self.parent().textEdit.toPlainText()
+        find_text = self.find_input.text()
+        if find_text:
+            index = text.find(find_text)
+            if index != -1:
+                self.parent().textEdit.moveCursor(1, QTextEdit.MoveAnchor)
+                self.parent().textEdit.moveCursor(3, QTextEdit.KeepAnchor)
+                self.parent().textEdit.setFocus()
+                self.parent().textEdit.ensureCursorVisible()
+
+    def replaceText(self):
+        text = self.parent().textEdit.toPlainText()
+        find_text = self.find_input.text()
+        replace_text = self.replace_input.text()
+        if find_text and replace_text:
+            text = text.replace(find_text, replace_text)
+            self.parent().textEdit.setPlainText(text)
+            QMessageBox.information(self, "Replacement", "Text replaced successfully.")
 
 if __name__ == '__main__':
     app = QApplication(sys.argv)
